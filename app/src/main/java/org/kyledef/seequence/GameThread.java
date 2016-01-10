495855e103b6a04ac5b3a461ffdcfe9d6edc07c6
Kyle E DeFreitas <kyle.e.defreitas@gmail.com>
2016-01-10 17:57:42 -0400

    Removed log used to debug errors

--- app/src/main/java/org/kyledef/seequence/GameThread.java
+++ app/src/main/java/org/kyledef/seequence/GameThread.java
@@ -39,7 +39,6 @@ class GameThread extends Thread {
 		Log.d(TAG, "Run Of thread initiated");
 		Canvas canvas = null;
 		while (isRunning) {
-			Log.d(TAG, "Looped");
 			canvas = surfaceHolder.lockCanvas();
 			if (canvas != null) {
 				phoneScreen.update();
@@ -47,20 +46,6 @@ class GameThread extends Thread {
 				timeElapsed = System.currentTimeMillis() - startTime;
 				surfaceHolder.unlockCanvasAndPost(canvas);
 			}
-
-//			if (pause){
-//				return; // Attempt to stop Thread
-//			}
-
-//			// check to see if to pause
-//			while (pause) {
-//				Log.d(TAG, "Paused");
-//				try {
-//					sleep(1000);
-//				} catch (InterruptedException e) {
-//					e.printStackTrace();
-//				}
-//			}
 		}
 	}
 }
