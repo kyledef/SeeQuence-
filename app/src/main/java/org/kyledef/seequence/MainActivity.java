7818c626bffd70dca1bc1d9dda80425645e108a4
shiv1994 <shivramx@gmail.com>
2016-01-15 23:35:42 -0400

    Google play games integrated, achievements can be unlocked and viewed at the moment but only from the main activity. Have to figure how to reference the mGoogleApiClient variable from a screen when running the game.

--- app/src/main/java/org/kyledef/seequence/MainActivity.java
+++ app/src/main/java/org/kyledef/seequence/MainActivity.java
@@ -1,26 +1,137 @@
 package org.kyledef.seequence;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 
-public class MainActivity extends Activity{
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.games.Games;
+import com.google.example.games.basegameutils.BaseGameUtils;
+
+import static org.kyledef.seequence.R.*;
+
+public class MainActivity extends Activity implements
+		GoogleApiClient.ConnectionCallbacks,
+		GoogleApiClient.OnConnectionFailedListener {
 
 	private static final String TAG = "MainActivity";
 	private ScreenManager phoneScreen = null;
+	public GoogleApiClient mGoogleApiClient;
+	private boolean mResolvingConnectionFailure = false;
+	private boolean mAutoStartSignInflow = true;
+	private boolean mSignInClicked = false;
+	private static int RC_SIGN_IN = 9001;
 
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		mGoogleApiClient = new GoogleApiClient.Builder(this)
+				.addConnectionCallbacks(this)
+				.addOnConnectionFailedListener(this)
+				.addApi(Games.API).addScope(Games.SCOPE_GAMES)
+				.build();
+
 		Log.i(TAG, "OnCreate of Main Activity called");
 		phoneScreen = new ScreenManager(this);
 		setContentView(phoneScreen);
 		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 		hideSystemUI();
+
+		signInClicked();
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		mGoogleApiClient.connect();
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		mGoogleApiClient.disconnect();
+	}
+
+	public void onConnected(Bundle connectionHint) {
+		// The player is signed in. Hide the sign-in button and allow the
+		// player to proceed.
+		Log.i("CONNECTED", "CODE!");
+
+		//Code just to test that api works and achievements are being shown.
+		Games.Achievements.unlock(mGoogleApiClient, this.getString(string.achievement_noob));
+
+		startActivityForResult(Games.Achievements.getAchievementsIntent(mGoogleApiClient),
+				100);
+	}
+
+	@Override
+	public void onConnectionFailed(ConnectionResult connectionResult) {
+		if (mResolvingConnectionFailure) {
+			// already resolving
+			return;
+		}
+
+		// if the sign-in button was clicked or if auto sign-in is enabled,
+		// launch the sign-in flow
+		if (mSignInClicked || mAutoStartSignInflow) {
+			mAutoStartSignInflow = false;
+			mSignInClicked = false;
+			mResolvingConnectionFailure = true;
+
+			// Attempt to resolve the connection failure using BaseGameUtils.
+			// The R.string.signin_other_error value should reference a generic
+			// error string in your strings.xml file, such as "There was
+			// an issue with sign-in, please try again later."
+			if (!BaseGameUtils.resolveConnectionFailure(this,
+					mGoogleApiClient, connectionResult,
+					RC_SIGN_IN, String.valueOf(string.signin_other_error))) {
+				mResolvingConnectionFailure = false;
+			}
+		}
+
+		// Put code here to display the sign-in button
+	}
+
+	@Override
+	public void onConnectionSuspended(int i) {
+		// Attempt to reconnect
+		mGoogleApiClient.connect();
+	}
+
+	protected void onActivityResult(int requestCode, int resultCode,
+									Intent intent) {
+		if (requestCode == RC_SIGN_IN) {
+			mSignInClicked = false;
+			mResolvingConnectionFailure = false;
+			if (resultCode == RESULT_OK) {
+				mGoogleApiClient.connect();
+			} else {
+				// Bring up an error dialog to alert the user that sign-in
+				// failed. The R.string.signin_failure should reference an error
+				// string in your strings.xml file that tells the user they
+				// could not be signed in, such as "Unable to sign in."
+				BaseGameUtils.showActivityResultError(this,
+						requestCode, resultCode, string.signin_failure);
+			}
+		}
+	}
+
+	private void signInClicked() {
+		mSignInClicked = true;
+		mGoogleApiClient.connect();
+	}
+
+	// Call when the sign-out button is clicked
+	private void signOutclicked() {
+		mSignInClicked = false;
+		Games.signOut(mGoogleApiClient);
 	}
 
 	protected void onResume() {
